5bfa86dab0f2f135a569357359aff0c4e31420c5
Sai Sri Harsha Pemmasani <sriharshapemmasani06@gmail.com>
2025-07-24 13:16:38 -0400

    added rainbowcolors to name

--- js/indexDesktopNavBar.js
+++ js/indexDesktopNavBar.js
@@ -1,4 +1,16 @@
-function closePopUP() {
+fetch('docs/desktopNavbar.txt')   // path to your file
+  .then(response => {
+    if (!response.ok) throw new Error('Network response was not ok');
+    return response.text(); // or .json() if it's JSON
+  })
+  .then(data => {
+    document.getElementById('nav-bar-top').innerHTML = data;
+  })
+  .catch(error => {
+    console.error('There was a problem with the fetch operation:', error);
+  });
+
+  function closePopUP() {
   const popupActionNavBar = document.getElementById("popedUpActionBar");
   popupActionNavBar.style.display = "none";
   console.log(" popupactionBar display set to none");
@@ -14,3 +26,4 @@ document.getElementById("scrollDown").addEventListener("click", () => {
     .getElementsByClassName("secondSection")[0]
     .scrollIntoView({ behavior: "smooth" });
 });
+
